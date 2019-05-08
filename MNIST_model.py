c6de29c4a6350bb24c6442a43734824d08d63ca3
Christopher <cgutierrez@mesosphere.io>
2019-05-08 14:22:53 -0700

    Add mlflow

--- MNIST_model.py
+++ MNIST_model.py
@@ -9,9 +9,9 @@ import os
 import subprocess
 import time
 import tempfile
+import mlflow
 
 if __name__ == "__main__":
-
     fashion_mnist = keras.datasets.fashion_mnist
     (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
