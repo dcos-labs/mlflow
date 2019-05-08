9b05124f337be0d1e93fd7077ce928815a08c1cf
Christopher <cgutierrez@mesosphere.io>
2019-05-08 14:26:23 -0700

    add mlflow.start_run()

--- MNIST_model.py
+++ MNIST_model.py
@@ -12,54 +12,52 @@ import tempfile
 import mlflow
 
 if __name__ == "__main__":
-    fashion_mnist = keras.datasets.fashion_mnist
-    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+    with mlflow.start_run():
+        fashion_mnist = keras.datasets.fashion_mnist
+        (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-    train_images = train_images / 255.0
-    test_images = test_images / 255.0
+        train_images = train_images / 255.0
+        test_images = test_images / 255.0
 
-    train_images = train_images.reshape(train_images.shape[0], 28, 28, 1)
-    test_images = test_images.reshape(test_images.shape[0], 28, 28, 1)
+        train_images = train_images.reshape(train_images.shape[0], 28, 28, 1)
+        test_images = test_images.reshape(test_images.shape[0], 28, 28, 1)
 
-    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-                   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+        class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                       'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-    print('\ntrain_images.shape: {}, of {}'.format(train_images.shape, train_images.dtype))
-    print('test_images.shape: {}, of {}'.format(test_images.shape, test_images.dtype))
+        print('\ntrain_images.shape: {}, of {}'.format(train_images.shape, train_images.dtype))
+        print('test_images.shape: {}, of {}'.format(test_images.shape, test_images.dtype))
 
+        model = keras.Sequential([
+          keras.layers.Conv2D(input_shape=(28,28,1), filters=8, kernel_size=3, 
+                              strides=2, activation='relu', name='Conv1'),
+          keras.layers.Flatten(),
+          keras.layers.Dense(10, activation=tf.nn.softmax, name='Softmax')
+        ])
+        model.summary()
 
-    model = keras.Sequential([
-      keras.layers.Conv2D(input_shape=(28,28,1), filters=8, kernel_size=3, 
-                          strides=2, activation='relu', name='Conv1'),
-      keras.layers.Flatten(),
-      keras.layers.Dense(10, activation=tf.nn.softmax, name='Softmax')
-    ])
-    model.summary()
+        testing = False
+        epochs = 5
 
-    testing = False
-    epochs = 5
+        model.compile(optimizer=tf.train.AdamOptimizer(), 
+                      loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
+        model.fit(train_images, train_labels, epochs=epochs)
 
-    model.compile(optimizer=tf.train.AdamOptimizer(), 
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
-    model.fit(train_images, train_labels, epochs=epochs)
+        test_loss, test_acc = model.evaluate(test_images, test_labels)
+        print('\nTest accuracy: {}'.format(test_acc))
 
-    test_loss, test_acc = model.evaluate(test_images, test_labels)
-    print('\nTest accuracy: {}'.format(test_acc))
+        MODEL_NAME = 'mnist'
+        MODEL_DIR = './' + MODEL_NAME
+        MODEL_VERSION = str(int(time.time()))
+        export_path = os.path.join(MODEL_DIR, str(MODEL_VERSION))
+        print('export_path = {}\n'.format(export_path))
+        if os.path.isdir(export_path):
+          print('\nAlready saved a model, cleaning up\n')
+          get_ipython().system('rm -r {export_path}')
 
-
-
-    MODEL_NAME = 'mnist'
-    MODEL_DIR = './' + MODEL_NAME
-    MODEL_VERSION = str(int(time.time()))
-    export_path = os.path.join(MODEL_DIR, str(MODEL_VERSION))
-    print('export_path = {}\n'.format(export_path))
-    if os.path.isdir(export_path):
-      print('\nAlready saved a model, cleaning up\n')
-      get_ipython().system('rm -r {export_path}')
-
-    tf.saved_model.simple_save(
-        keras.backend.get_session(),
-        export_path,
-        inputs={'input_image': model.input},
-        outputs={t.name:t for t in model.outputs})
+        tf.saved_model.simple_save(
+            keras.backend.get_session(),
+            export_path,
+            inputs={'input_image': model.input},
+            outputs={t.name:t for t in model.outputs})
